27205c0851f1b12e3add5c55dbe21ac19fa62e7f
Fabian Pijcke <fabian.pijcke@gmail.com>
2023-08-29 18:26:45 +0200

    other configurations

--- variants/java-gradle/build.gradle.kts
+++ variants/java-gradle/build.gradle.kts
@@ -1,19 +1,27 @@
 // https://docs.gradle.org/current/userguide/cross_project_publications.html
-task("runConsoleNoAi") {
-    val projectsToLoad = listOf(":mainConsole", ":rules")
 
-    projectsToLoad.forEach { dependsOn("$it:jar") }
+// The first project to load is the runner (e.g., :mainconsole, :mainjavafx, :mainnetwork)
+data class NimBuild(val name : String, val projectsToLoad : List<String>)
 
-    exec { commandLine("rm", "-rf", "runConsoleNoAi") }
-    exec { commandLine("mkdir", "runConsoleNoAi") }
-    copy {
-        from(projectsToLoad.map { project(it).buildDir.resolve("libs") })
-        into("runConsoleNoAi")
+listOf(
+    NimBuild("runConsoleNoAi", listOf(":mainconsole", ":rules")),
+//    NimBuild("runConsoleAllAis", listOf(":mainConsole", ":rules", ":aione", ":aibest", ":airandom", ":aimax")),
+//    NimBuild("runConsoleEasyAis", listOf(":mainConsole", ":rules", ":aione", ":aimax")),
+).forEach {
+    tasks.register(it.name) {
+        it.projectsToLoad.forEach { dependsOn("$it:jar") }
+
+        exec { commandLine("rm", "-rf", "runConsoleNoAi") }
+        exec { commandLine("mkdir", "runConsoleNoAi") }
+        copy {
+            from(it.projectsToLoad.map { project(it).buildDir.resolve("libs") })
+            into("runConsoleNoAi")
+        }
+        exec { commandLine(
+            "java",
+            "--module-path", "runConsoleNoAi",
+            "--add-modules", "ALL-MODULE-PATH",
+            "-jar", "runConsoleNoAi/" + project(it.projectsToLoad.first()).buildDir.resolve("libs").list()?.first()
+        )}
     }
-    exec { commandLine(
-        "java",
-        "--module-path", "runConsoleNoAi",
-        "--add-modules", "ALL-MODULE-PATH",
-        "-jar", "runConsoleNoAi/" + project(projectsToLoad.first()).buildDir.resolve("libs").list()?.first()
-    )}
 }
