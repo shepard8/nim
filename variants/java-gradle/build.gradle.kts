d04571121295e030e12f671101921893bb4d7776
Fabian Pijcke <fabian.pijcke@gmail.com>
2023-08-29 18:12:56 +0200

    runConsoleNoAi configuration

--- variants/java-gradle/build.gradle.kts
+++ variants/java-gradle/build.gradle.kts
@@ -1,6 +1,19 @@
- // https://docs.gradle.org/current/userguide/cross_project_publications.html
-tasks.register("run-console-no-ai") {
-    doLast {
+// https://docs.gradle.org/current/userguide/cross_project_publications.html
+task("runConsoleNoAi") {
+    val projectsToLoad = listOf(":mainConsole", ":rules")
 
+    projectsToLoad.forEach { dependsOn("$it:jar") }
+
+    exec { commandLine("rm", "-rf", "runConsoleNoAi") }
+    exec { commandLine("mkdir", "runConsoleNoAi") }
+    copy {
+        from(projectsToLoad.map { project(it).buildDir.resolve("libs") })
+        into("runConsoleNoAi")
     }
+    exec { commandLine(
+        "java",
+        "--module-path", "runConsoleNoAi",
+        "--add-modules", "ALL-MODULE-PATH",
+        "-jar", "runConsoleNoAi/" + project(projectsToLoad.first()).buildDir.resolve("libs").list()?.first()
+    )}
 }
